837ada9f08f390dec33ade25cb103ab247ff7544
fgomezvalverde <fafa_gttm@hotmail.com>
2014-04-04 21:56:18 -0600

    Cambio a Eclipse

---
+++ Compilador/src/scanner/Token.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scanner;
+
+/**
+ *
+ * @author curso
+ */
+public enum Token {
+    IDENTIFICADOR, INT, SUMA, RESTA, DIV, MULT, ASSIGN, ERROR
+}
